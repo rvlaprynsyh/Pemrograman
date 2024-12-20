59c167fac6faa96c9dd3682e7e074cf9e8b0a71e
rvlaprynsyh <muhammadrival2708@gmail.com>
2024-12-20 19:00:05 +0700

    Add files via upload

---
+++ stylep15.js
@@ -0,0 +1,33 @@
+document.write("<hr size=3 color=red>");
+
+document.write("<h1 style='text-align: center;'>WEBSITE Profile <a href='John_Due.html' style='color: blue; text-decoration: none;'>John Due</a></h1>");
+
+document.write("<hr size=3 color=red>");
+
+document.write("<center><img src='johndue.png' width=200 height=200 style='margin: 20px 0;'></center>");
+
+document.write("<div style='text-align: left; font-size: 16px; color: black;'>");
+document.write("<ol type='1'>");
+document.write("<li>Nim : 123456789</li>");
+document.write("<li>Nama : John Due</li>");
+document.write("<li>Kelas : 01SIFP099</li>");
+document.write("</ol>");
+document.write("</div>");
+
+document.write("<div style='text-align: left; font-size: 16px; color: black;'>");
+document.write("<ul type='circle'>");
+document.write("<li>Program Studi : Sistem Informasi</li>");
+document.write("<li>Alamat : JL. Taman Puring, Tangerang Selatan</li>");
+document.write("<li>Asal Sekolah : SMK ADIYASA INFORMASI</li>");
+document.write("</ul>");
+document.write("</div>");
+
+document.write("<div style='text-align: left; font-size: 16px; color: black;'>");
+document.write("<ol type='I'>");
+document.write("<li>Jurusan : MULTIMEDIA</li>");
+document.write("<li>Email : john.due@gmail.com</li>");
+document.write("<li>Telepon : 0812-1234-5678</li>");
+
+document.write("<hr size=3 color=red>");
+document.write("<div style='text-align: center; font-size: 16px;'>Designed by John Due &copy; 2024 Allright Reserved</div>");
+document.write("<hr size=3 color=red>");
